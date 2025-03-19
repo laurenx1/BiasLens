ac524ff83e7881a5d014e7a8c7bba2abb5675af6
lpryor <lpryor@caltech.edu>
2025-03-19 19:08:55 -0400

    added requirements.txt

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Comparing bias and sensationalism in AI vs. web recommendations of news articles through their ability to grab the attention of college students.
 
 
-
 ## Data
 Our data comes from three sources: a survey of current Caltech students using Airtable to collect information about their health news preferences. Additionally, we generated data to populate the student data ntoh manually and using DeepSeek. Finally, we include in our frontend web app a way to take the survey and continue populating the database. 
 
---
+++ biaslensdb/setup-routines.sql
@@ -0,0 +1,29 @@
+-- procedural sql
+-- optional trigger included in DDL as allowed by spec. 
+
+-- required 1 UDF
+-- function to return a student's avg sensation score by UID
+-- param: uid
+
+
+-- function to return a student's avg sensation score by username 
+-- param: username
+
+
+-- required 1 procedure
+-- choose what type of ranking that you want to see
+-- param: string choice, eg. house, major, year of graduation
+-- we can see the ranking of best to worst house, major, year of graduation
+
+
+-- see self stats 
+-- procedure to see:  your ranking, average score, and total score
+-- ranking within major, ranking within year, and ranking within house
+
+
+
+
+
+-- see top x
+-- param: x how much of the top list can you see
+-- error handling: can't be more than in the student's table
